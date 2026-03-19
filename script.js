cf62a5c8ad1b529405ce23f820597b61cf0ad00e
reypdf <269479476+ReyPDF@users.noreply.github.com>
2026-03-19 22:30:54 +0800

    test

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-let allFiles = [];
-let filteredFiles = [];
-let currentPage = 1;
-const perPage = 10;
-
-fetch('data/files.json')
-  .then(res => res.json())
-  .then(data => {
-    allFiles = data.sort((a, b) => a.name.localeCompare(b.name));
-    filteredFiles = allFiles;
-    render();
-  });
-
-document.getElementById('search').addEventListener('input', (e) => {
-  const value = e.target.value.toLowerCase();
-
-  filteredFiles = allFiles.filter(file =>
-    file.name.toLowerCase().includes(value)
-  );
-
-  currentPage = 1;
-  render();
-});
-
 function render() {
   const container = document.getElementById('pdf-container');
   const pagination = document.getElementById('pagination');
@@ -35,9 +11,12 @@ function render() {
     const div = document.createElement('div');
 
     div.innerHTML = `
-      <h3>${file.name}</h3>
+      <h3><a href="${file.page}">${file.name}</a></h3>
+
       <iframe src="${file.file}" width="300" height="200"></iframe>
+
       <br>
+      <a href="${file.page}">View Details</a> |
       <a href="${file.file}" download>Download</a>
       <hr>
     `;
@@ -60,13 +39,10 @@ function render() {
 
     pagination.appendChild(btn);
   }
-}
 
-div.innerHTML = `
-  <h3>${file.name}</h3>
-  <iframe src="${file.file}" width="300" height="200"></iframe>
-  <br>
-  <a href="${file.page}">View Page</a>
-  <a href="${file.file}" download>Download</a>
-  <hr>
-`;
+  // ✅ SEO hidden links (IMPORTANT)
+  const allLinksDiv = document.getElementById('all-links');
+  allLinksDiv.innerHTML = allFiles.map(file => 
+    `<a href="${file.page}">${file.name}</a>`
+  ).join('');
+}
